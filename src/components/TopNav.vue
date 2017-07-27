e41c8766ae919ef860f805f7232ae1f9317ba495
ETY001 <ety001@domyself.me>
2017-07-27 23:29:21 +0800

    change tutorial from local to shimo

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -3,7 +3,7 @@
     <el-col :span="20" :offset="2">
       <el-menu theme="dark" :default-active="activeIndex" :router="true" class="el-menu-demo" mode="horizontal" @select="handleSelect">
         <el-menu-item index="/">首页</el-menu-item>
-        <el-menu-item index="tutorial">教程</el-menu-item>
+        <li class="el-menu-item"><a href="https://shimo.im/doc/pghTlewrLxsYiHD3?r=V7XL4/" target="_blank">教程</a></li>        
       </el-menu>
     </el-col>
   </el-row>
