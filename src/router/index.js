45b7249b5be236eb0f66fd8434e5d4f46a4d960d
ETY001 <ety001@domyself.me>
2017-10-29 12:07:50 +0800

    remove hello

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
 import Home from '@/components/Home';
 
 Vue.use(Router);
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
-    },
-    {
-      path: '/hello',
-      name: 'hello',
-      component: Hello,
     },
   ],
 });
