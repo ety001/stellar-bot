1b8f4bc87d42425ca56155ed340a224b904e9fc3
ety001 <ety001@domyself.me>
2018-04-28 03:58:15 +0800

    update version number

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import transEn from './i18n/en.json';
 import transZhCn from './i18n/zh-CN.json';
 import Api from './lib/Api';
 
-const VERSION = '0.0.8-1';
+const VERSION = '0.0.9';
 const intervalTime = 5;
 // const robotIntervalTime = 20;
 const priceSensitivity = 4;
